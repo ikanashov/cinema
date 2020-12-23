2b06a1c40576140294dfd0f61213e51805feaf36
Ivan Kanashov <ikanashov@gmail.com>
2020-12-23 15:49:35 +0500

    script to get data from imdb.com

---
+++ db/get_imdb_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Скачиваем необходимые файлы IMDb data set
+wget -c -t 5 https://datasets.imdbws.com/name.basics.tsv.gz
+
+wget -c -t 5 https://datasets.imdbws.com/title.basics.tsv.gz
+
+wget -c -t 5 https://datasets.imdbws.com/title.episode.tsv.gz
+
+gzip -df name.basics.tsv.gz
+
+gzip -df title.basics.tsv.gz
+
+gzip -df title.episode.tsv.gz
+
+
