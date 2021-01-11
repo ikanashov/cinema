04f6b649d1df2fe805016437b52416f6b370b360
Ivan Kanashov <ikanashov@gmail.com>
2021-01-11 18:04:23 +0500

    create django app

--- cinema_admin/config/settings/base.py
+++ cinema_admin/config/settings/base.py
@@ -9,26 +9,22 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hma%kuj)*uncn8&d2r+o$2zee4$mv*nb6im&v7cd!)#mkjubrp'
+SECRET_KEY = 'gqoit08iq*f(3p#*sw_^chbma)w^you^nfrodm2xe803xa=#tk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'dev.usurt.ru',
+]
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cinema',
 ]
 
 MIDDLEWARE = [
@@ -70,19 +67,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -100,10 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -113,8 +105,5 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
--- cinema_admin/manage.py
+++ cinema_admin/manage.py
@@ -3,10 +3,27 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
+LOCALENVIROMENTPATH = '../'
+
+
+def load_local_env():
+    envfile = LOCALENVIROMENTPATH + '.env'
+    dotenv_path = os.path.join(os.path.dirname(__file__), envfile)
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)
+    else:
+        print()
+        print('!!!WARNING!!!')
+        print(f'Local environment file (.env) not found in path:"{LOCALENVIROMENTPATH}".')
+        print('Default configuration will be used.')
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
+    load_local_env()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
