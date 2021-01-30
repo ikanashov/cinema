98ffd9bec74c40c7731f89ae8f2255d4635aa30d
Ivan Kanashov <ikanashov@gmail.com>
2021-01-30 22:44:57 +0500

    cinema django signals and debug tool example

--- cinema_admin/cinema/apps.py
+++ cinema_admin/cinema/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class CinemaConfig(AppConfig):
     name = 'cinema'
+
+    def ready(self):
+        print ('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        import cinema.signals
--- cinema_admin/config/settings/dev.py
+++ cinema_admin/config/settings/dev.py
@@ -1,3 +1,46 @@
 from .base import *
 
 DEBUG = True
+
+INSTALLED_APPS += [
+    'django_extensions',
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]',
+        },
+    },
+    'handlers': {
+        'debug-console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+            'filters': ['require_debug_true'],
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['debug-console'],
+            'propagate': False,
+        }
+    },
+}
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+    '172.18.120.3',
+]
